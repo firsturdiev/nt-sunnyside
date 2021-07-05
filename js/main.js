165e0ee6cba50336becfd6c79a2380136336eeab
Samandar Turdiev <firsturdiev@gmail.com>
2021-07-05 21:39:57 +0500

    Adding site-header, hero, features and services sections

---
+++ js/main.js
@@ -0,0 +1,8 @@
+// menu 
+
+const elMenuToggle = document.querySelector('.menu')
+const elMenu = document.querySelector('.sitenav');
+
+elMenuToggle.addEventListener('click', function() {
+  elMenu.classList.toggle('sitenav--open')
+})
